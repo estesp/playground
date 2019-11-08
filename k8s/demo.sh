f7a8f1decbdf2bed4386fcb49a789765d715862e
Phil Estes <estesp@linux.vnet.ibm.com>
2019-11-08 15:44:18 -0500

    Demo and examples for talk
    Signed-off-by: Phil Estes <estesp@gmail.com>

---
+++ k8s/demo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+kubectl apply -f playground-deployment.yaml
+kubectl expose deployment playground-deployment --type=NodePort
+kubectl describe svc/playground-deployment
+
+#forkbomb: :(){ :|: & };:
+# playground: http://169.61.166.248:$NODEPORT

771721e202027cc74bf66e3d33b0ed8e818f06f7
Phil Estes <estesp@gmail.com>
2020-01-28 17:19:46 +0200

    Some cleanup of demo scripts/yaml
    Signed-off-by: Phil Estes <estesp@gmail.com>

--- k8s/cleanup.sh
+++ k8s/cleanup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+_YAML="playground-deployment.yaml"
+[ -n "${1}" ] && {
+ _YAML="playground-deployment-${1}.yaml"
+}
+
 kubectl delete svc/playground-deployment
-kubectl delete -f playground-deployment.yaml
+kubectl delete -f ${_YAML}
--- k8s/demo.sh
+++ k8s/demo.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-kubectl apply -f playground-deployment.yaml
+
+_YAML="playground-deployment.yaml"
+[ -n "${1}" ] && {
+ _YAML="playground-deployment-${1}.yaml"
+}
+
+echo ">> kubectl apply -f ${_YAML}"
+kubectl apply -f ${_YAML}
+echo ">> kubectl expose deployment playground-deployment --type=NodePort"
 kubectl expose deployment playground-deployment --type=NodePort
+echo ">> kubectl describe svc/playground-deployment"
 kubectl describe svc/playground-deployment
 
 #forkbomb: :(){ :|: & };:
